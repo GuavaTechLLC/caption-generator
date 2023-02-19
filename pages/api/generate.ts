89ed773015812b26afdc1047cd2c86ec3c5cab62
Sarah Amann <sarah@Sarahs-MacBook-Pro.local>
2023-02-19 13:03:40 -0600

    working on replicate api call

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -41,7 +41,7 @@ export default async function handler(
 
   const imageUrl = req.body.imageUrl;
   // POST request to Replicate to start the image restoration generation process
-  let startResponse = await fetch("https://api.replicate.com/v1/predictions", {
+  let response = await fetch("https://api.replicate.com/v1/predictions", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -49,37 +49,74 @@ export default async function handler(
     },
     body: JSON.stringify({
       version:
-        "9283608cc6b7be6b65a8e44983db012355fde4132009bf99d976b2f0896856a3",
-      input: { img: imageUrl, version: "v1.4", scale: 2 },
+        "2e1dddc8621f72155f24cf2e0adbde548458d3cab9f00c0139eea840d0ac4746",
+      input: { image: imageUrl },
     }),
   });
 
-  let jsonStartResponse = await startResponse.json();
-  let endpointUrl = jsonStartResponse.urls.get;
+  let prediction = await response.json();
+  let endpointUrl = prediction.urls.get;
+
+
+  // if (response.status !== 201) {
+  //   let error = await response.json();
+  //   // res.statusCode = 500;
+  //   // res.end(JSON.stringify({ detail: error.detail }));
+  //   return error;
+  // }
+
+  if (endpointUrl) {
+    const finalResponse = await fetch(
+      endpointUrl,
+      {
+        headers: {
+          Authorization: `Token ${process.env.REPLICATE_API_TOKEN}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    // if (finalResponse.status !== 200) {
+    //   let error = await finalResponse.json();
+    //   // res.statusCode = 500;
+    //   // res.end(JSON.stringify({ detail: error.detail }));
+    //   return error;
+    // }
+  
+    const descriptionResult = await finalResponse.json();
+    console.log('IS IT HERE:', descriptionResult)
+    res.end(JSON.stringify(descriptionResult));
+  }
+
+  // const prediction = await response.json();
+  // res.statusCode = 201;
+  // console.log(JSON.stringify(prediction))
+  // res.end(JSON.stringify(prediction));
+
 
   // GET request to get the status of the image restoration process & return the result when it's ready
-  let restoredImage: string | null = null;
-  while (!restoredImage) {
-    // Loop in 1s intervals until the alt text is ready
-    console.log("polling for result...");
-    let finalResponse = await fetch(endpointUrl, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Token " + process.env.REPLICATE_API_KEY,
-      },
-    });
-    let jsonFinalResponse = await finalResponse.json();
+  // let restoredImage: string | null = null;
+  // while (!restoredImage) {
+  //   // Loop in 1s intervals until the alt text is ready
+  //   console.log("polling for result...", endpointUrl);
+  //   let finalResponse = await fetch(endpointUrl, {
+  //     method: "GET",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       Authorization: "Token " + process.env.REPLICATE_API_KEY,
+  //     },
+  //   });
+  //   let jsonFinalResponse = await finalResponse.json();
 
-    if (jsonFinalResponse.status === "succeeded") {
-      restoredImage = jsonFinalResponse.output;
-    } else if (jsonFinalResponse.status === "failed") {
-      break;
-    } else {
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-    }
-  }
-  res
-    .status(200)
-    .json(restoredImage ? restoredImage : "Failed to restore image");
+  //   if (jsonFinalResponse.status === "succeeded") {
+  //     console.log('SUCCESS', jsonFinalResponse)
+  //     // restoredImage = jsonFinalResponse.output;
+  //   } else if (jsonFinalResponse.status === "failed") {
+  //     break;
+  //   } else {
+  //     await new Promise((resolve) => setTimeout(resolve, 1000));
+  //   }
+  // }
+  // res
+  //   .status(200)
+  //   .json(restoredImage ? restoredImage : "Failed to restore image");
 }
